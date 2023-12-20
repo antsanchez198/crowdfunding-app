06cf671321c80bab2c4d6e2a82189e101ffd6aec
Anthony Sanchez <antsanchez198@gmail.com>
2023-12-19 23:46:28 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import imageHeroDesktop from "./images/image-hero-desktop.jpg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <img src={imageHeroDesktop}></img>
+      <div className="container">
+        {/* Section 1 */}
+        <section>
+          <h2>Mastercraft Bamboo Monitor Riser</h2>
+          <p>
+            A beautiful & handcrafted monitor stand to reduce neck and eye
+            strain.
+          </p>
+          <div className="button-containers">
+            <button>Back this project</button>
+            <button>Bookmark</button>
+          </div>
+        </section>
+
+        {/* Section 2 */}
+        <section>
+          <div className="statistics-container">
+            
+            <div>
+              <h2>$89,914</h2>
+              <p>of $100,000 backed</p>
+            </div>
+            <span></span>
+
+            <div>
+              <h2>$89,914</h2>
+              <p>total backers</p>
+            </div>
+            <span></span>
+
+            <div>
+              <h2>$89,914</h2>
+              <p>days left</p>
+            </div>
+          </div>
+
+          <progress className="progressBar" value="75" max="100">
+            32%
+          </progress>
+        </section>
+
+        <section>
+          <h3>About this Project</h3>
+          <p>Something goes here</p>
+
+          <div>
+            <div>
+              <div>
+                <h3>Bamboo Stand</h3>
+                <p>Pledge $25 or more</p>
+              </div>
+
+              <p>You get an ergoomic stand</p>
+
+              <div>
+                <div>
+                  <h2>101</h2>
+                  <p>left</p>
+                </div>
+                <button>Select Reawrd</button>
+              </div>
+            </div>
+
+            <div>
+              <div>
+                <h3>Bamboo Stand</h3>
+                <p>Pledge $25 or more</p>
+              </div>
+
+              <p>You get an ergoomic stand</p>
+
+              <div>
+                <div>
+                  <h2>101</h2>
+                  <p>left</p>
+                </div>
+                <button>Select Reawrd</button>
+              </div>
+            </div>
+
+            <div>
+              <div>
+                <h3>Bamboo Stand</h3>
+                <p>Pledge $25 or more</p>
+              </div>
+
+              <p>You get an ergoomic stand</p>
+
+              <div>
+                <div>
+                  <h2>101</h2>
+                  <p>left</p>
+                </div>
+                <button>Select Reawrd</button>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </body>
   );
 }
 
