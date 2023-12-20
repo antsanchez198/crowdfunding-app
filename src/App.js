3c6eb870948a01b1ff2818f2cda4fa2244c252a6
Anthony Sanchez <antsanchez198@gmail.com>
2023-12-20 00:36:30 -0500

    fixed up some design changes

--- src/App.js
+++ src/App.js
@@ -42,63 +42,64 @@ function App() {
           </div>
 
           <progress className="progressBar" value="75" max="100">
-            32%
           </progress>
         </section>
 
         <section>
           <h3>About this Project</h3>
-          <p>Something goes here</p>
+          <p>The Mastercraft Bamboo Monitor Riser is a sturdy and stylish platform that elevates your screen to a more comfortable viewing height. Placing your monitor at eye level has the potential to improve your posture and make you more comfortable while at work, helping you stay focused on the task at hand.</p>
+          <p>Featuring artisan craftsmanship, the simplicity of design creates extra desk space below your computer to allow notepads, pens, and USB sticks to be stored under the stand.</p>
 
-          <div>
+          <div className="rewardsContainer">
             <div>
-              <div>
+              <div className="row-direction">
                 <h3>Bamboo Stand</h3>
                 <p>Pledge $25 or more</p>
               </div>
 
-              <p>You get an ergoomic stand</p>
+              <p>You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you'll be added to a special Backer member list.</p>
 
-              <div>
+              <div className="row-direction">
                 <div>
                   <h2>101</h2>
                   <p>left</p>
                 </div>
-                <button>Select Reawrd</button>
+                <button>Select Reward</button>
               </div>
             </div>
 
             <div>
-              <div>
-                <h3>Bamboo Stand</h3>
-                <p>Pledge $25 or more</p>
+              <div className="row-direction">
+                <h3>Black Edition Stand</h3>
+                <p>Pledge $75 or more</p>
               </div>
 
-              <p>You get an ergoomic stand</p>
+              <p>You get a Black Special Edition computer stand and a personal thank you. You'll be added to our Backer member list. Shipping is included.
+</p>
 
-              <div>
+              <div className="row-direction">
                 <div>
-                  <h2>101</h2>
+                  <h2>64</h2>
                   <p>left</p>
                 </div>
-                <button>Select Reawrd</button>
+                <button>Select Reward</button>
               </div>
             </div>
 
             <div>
-              <div>
-                <h3>Bamboo Stand</h3>
-                <p>Pledge $25 or more</p>
+              <div className="row-direction">
+                <h3>Mahogany Special Edition</h3>
+                <p>Pledge $200 or more</p>
               </div>
 
-              <p>You get an ergoomic stand</p>
+              <p>You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You'll be added to our Backer member list. Shipping is included.</p>
 
-              <div>
+              <div className="row-direction">
                 <div>
-                  <h2>101</h2>
+                  <h2>0</h2>
                   <p>left</p>
                 </div>
-                <button>Select Reawrd</button>
+                <button>Out of Stock</button>
               </div>
             </div>
           </div>
